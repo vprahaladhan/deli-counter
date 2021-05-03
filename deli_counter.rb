4b80a109dabb7fb75a3ff27803925f5fe1f9c0c5
prahal <vprahaladhan@gmail.com>
2021-05-03 22:00:53 +1000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line(deli)
+  if deli.size == 0 then
+    puts "The line is currently empty."
+  else
+    queue = "The line is currently:"
+    deli.each_with_index do |person, index|
+      queue << " #{index + 1}. #{person}"
+    end
+    puts queue
+  end
+end
+
+def take_a_number(deli, name)
+  puts "Welcome, #{name}. You are number #{deli.size() + 1} in line."
+end
